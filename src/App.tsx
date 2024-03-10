856ed61f81c0039d829545b190765516281d1cdd
Eduard Plechkens <work@plechkens.com>
2024-03-10 19:13:55 +0300

    refactor: remove unnecessary panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState, useEffect, ReactNode } from 'react';
-import bridge, { UserInfo } from '@vkontakte/vk-bridge';
-import { View, SplitLayout, SplitCol, ScreenSpinner } from '@vkontakte/vkui';
-import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
+import { useState, useEffect, ReactNode } from "react";
+import bridge, { UserInfo } from "@vkontakte/vk-bridge";
+import { View, SplitLayout, SplitCol, ScreenSpinner } from "@vkontakte/vkui";
+import { useActiveVkuiLocation } from "@vkontakte/vk-mini-apps-router";
 
-import { Persik, Home } from './panels';
-import { DEFAULT_VIEW_PANELS } from './routes';
+import { Home } from "./panels";
+import { DEFAULT_VIEW_PANELS } from "./routes";
 
 export const App = () => {
-  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
+  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } =
+    useActiveVkuiLocation();
   const [fetchedUser, setUser] = useState<UserInfo | undefined>();
-  const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
+  const [popout, setPopout] = useState<ReactNode | null>(
+    <ScreenSpinner size="large" />
+  );
 
   useEffect(() => {
     async function fetchData() {
-      const user = await bridge.send('VKWebAppGetUserInfo');
+      const user = await bridge.send("VKWebAppGetUserInfo");
       setUser(user);
       setPopout(null);
     }
@@ -25,7 +28,6 @@ export const App = () => {
       <SplitCol>
         <View activePanel={activePanel}>
           <Home id="home" fetchedUser={fetchedUser} />
-          <Persik id="persik" />
         </View>
       </SplitCol>
     </SplitLayout>
--- src/routes.ts
+++ src/routes.ts
@@ -4,23 +4,17 @@ import {
   createRoot,
   createView,
   RoutesConfig,
-} from '@vkontakte/vk-mini-apps-router';
+} from "@vkontakte/vk-mini-apps-router";
 
-export const DEFAULT_ROOT = 'default_root';
+export const DEFAULT_ROOT = "default_root";
 
-export const DEFAULT_VIEW = 'default_view';
+export const DEFAULT_VIEW = "default_view";
 
-export const DEFAULT_VIEW_PANELS = {
-  HOME: 'home',
-  PERSIK: 'persik',
-} as const;
+export const DEFAULT_VIEW_PANELS = { HOME: "home" } as const;
 
 export const routes = RoutesConfig.create([
   createRoot(DEFAULT_ROOT, [
-    createView(DEFAULT_VIEW, [
-      createPanel(DEFAULT_VIEW_PANELS.HOME, '/', []),
-      createPanel(DEFAULT_VIEW_PANELS.PERSIK, `/${DEFAULT_VIEW_PANELS.PERSIK}`, []),
-    ]),
+    createView(DEFAULT_VIEW, [createPanel(DEFAULT_VIEW_PANELS.HOME, "/", [])]),
   ]),
 ]);
 
