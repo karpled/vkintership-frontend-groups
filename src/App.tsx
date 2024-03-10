d7d9af06a02682001ecd06b5b95c522fbf6213cf
Eduard Plechkens <work@plechkens.com>
2024-03-10 19:26:54 +0300

    chore: update eslint and esm migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect, ReactNode } from "react";
-import bridge, { UserInfo } from "@vkontakte/vk-bridge";
-import { View, SplitLayout, SplitCol, ScreenSpinner } from "@vkontakte/vkui";
+import { View, SplitLayout, SplitCol } from "@vkontakte/vkui";
 import { useActiveVkuiLocation } from "@vkontakte/vk-mini-apps-router";
 
 import { Home } from "./panels";
@@ -9,25 +7,12 @@ import { DEFAULT_VIEW_PANELS } from "./routes";
 export const App = () => {
   const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } =
     useActiveVkuiLocation();
-  const [fetchedUser, setUser] = useState<UserInfo | undefined>();
-  const [popout, setPopout] = useState<ReactNode | null>(
-    <ScreenSpinner size="large" />
-  );
-
-  useEffect(() => {
-    async function fetchData() {
-      const user = await bridge.send("VKWebAppGetUserInfo");
-      setUser(user);
-      setPopout(null);
-    }
-    fetchData();
-  }, []);
 
   return (
-    <SplitLayout popout={popout}>
+    <SplitLayout>
       <SplitCol>
         <View activePanel={activePanel}>
-          <Home id="home" fetchedUser={fetchedUser} />
+          <Home id="home" />
         </View>
       </SplitCol>
     </SplitLayout>
